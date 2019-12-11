1000a5c1a3ff4adb4eef8fc80bf02cc0736b4973
Alex Akimov <54820232+alexwashpe@users.noreply.github.com>
2019-12-11 18:21:24 +0300

    Update server.py

--- src/server.py
+++ src/server.py
@@ -16,19 +16,25 @@ class ServerProtocol(LineOnlyReceiver):
     factory: 'Server'
     login: str = None
 
+    def send_history(self):
+        for msg in self.factory.history[-10::]:
+            self.sendLine(msg.encode())
+
     def connectionMade(self):
         # Потенциальный баг для внимательных =)
         self.factory.clients.append(self)
+        print(f"Users connected: {len(self.factory.clients)}")
 
     def connectionLost(self, reason=connectionDone):
         self.factory.clients.remove(self)
+        print(f"Users connected: {len(self.factory.clients)}")
 
     def lineReceived(self, line: bytes):
-        content = line.decode()
+        content = line.decode(encoding="utf-8", errors="ignore")
 
         if self.login is not None:
             content = f"Message from {self.login}: {content}"
-
+            self.factory.history.append(content)
             for user in self.factory.clients:
                 if user is not self:
                     user.sendLine(content.encode())
@@ -36,7 +42,17 @@ class ServerProtocol(LineOnlyReceiver):
             # login:admin -> admin
             if content.startswith("login:"):
                 self.login = content.replace("login:", "")
-                self.sendLine("Welcome!".encode())
+                if self.factory.logins:
+                    if self.login in self.factory.logins:
+                        self.sendLine(f"Login {self.login} is busy! Try another login.".encode())
+                        self.login = None
+                    else:
+                        self.factory.logins.append(self.login)
+                        self.sendLine("Welcome!".encode())
+                        self.send_history()
+                else:
+                    self.factory.logins.append(self.login)
+                    self.sendLine("Welcome!".encode())
             else:
                 self.sendLine("Invalid login".encode())
 
@@ -44,9 +60,13 @@ class ServerProtocol(LineOnlyReceiver):
 class Server(ServerFactory):
     protocol = ServerProtocol
     clients: list
+    history: list
+    logins: list
 
     def startFactory(self):
         self.clients = []
+        self.history = []
+        self.logins = []
         print("Server started")
 
     def stopFactory(self):
